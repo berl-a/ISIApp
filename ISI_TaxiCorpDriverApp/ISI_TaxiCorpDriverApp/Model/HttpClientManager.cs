a4b83722dd29f288414aac690fb37d65c2511a71
dawid_szewczyk <dawid.szewczyk19@gmail.com>
2020-04-19 23:08:25 +0200

    TaxiDriverApp WPF

---
+++ ISI_TaxiCorpDriverApp/ISI_TaxiCorpDriverApp/Model/HttpClientManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISI_TaxiCorpDriverApp.Model
+{
+    static class HttpClientManager
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<HttpResponseMessage> PostAsync(string requestUri, FormUrlEncodedContent content) {
+            return await client.PostAsync(requestUri, content);
+        }
+
+        public static async Task<string> BuiilRequestUri(string requestUri, FormUrlEncodedContent content) {
+            string parameters = await content.ReadAsStringAsync();
+
+            return string.Format("{0}?{1}", requestUri, parameters);
+        }
+    }
+}
